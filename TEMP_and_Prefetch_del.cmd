392bdb93b121c86cd79c4f5ab4664124573ab162
SMTEmon <156873907+SMTEmon@users.noreply.github.com>
2024-01-21 20:49:55 +0600

    Update TEMP_and_Prefetch_del.cmd

--- TEMP_and_Prefetch_del.cmd
+++ TEMP_and_Prefetch_del.cmd
@@ -1,4 +1,14 @@
 @echo off
+:: Check if the script has administrative privileges
+NET SESSION >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else (
+    echo Running with administrative privileges.
+)
+
+:: Your existing cleanup script starts here
 cd /D %temp%
 for /d %%D in (*) do rd /s /q "%%D"
 del /f /q *.*
@@ -8,3 +18,11 @@ cd /D C:\Windows\Temp
 del /f /q *.*
 cd /D C:\Users\SMTEmon\AppData\Local\Temp
 del /f /q *.*
+goto :EOF
+
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\RunElevated.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\RunElevated.vbs"
+"%temp%\RunElevated.vbs"
+del /f /q "%temp%\RunElevated.vbs"
+goto :EOF
